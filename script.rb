d0e0b6038fb2444fa300d2c1c03e612735e5c5e2
Daniel Salazar <disalazarg@gmail.com>
2016-11-11 02:10:52 -0300

    Scripty

--- script.rb
+++ script.rb
@@ -1,1 +1,31 @@
-puts "Hello, World"
+require 'csv'
+require 'date'
+
+class Email
+  attr_accessor :email, :date, :batch
+
+  def initialize(email, date, batch)
+    @email = email
+    @date  = DateTime.parse date
+    @batch = batch.to_i
+  end
+
+  def to_s
+    "#{@email}, #{@date}"
+  end
+
+  def to_sql
+    "(('#{@email}'), '#{@date}', 2, #{@batch})"
+  end
+end
+
+list = Array.new
+CSV.foreach 'mails.csv' do |row|
+  list << Email.new(*row) if row.last =~ /\A\d+\z/
+end
+
+unless Batch.any?
+  list.map(&:batch).uniq.map do |i|
+    Batch.create(product_id: 2)
+  end
+end
